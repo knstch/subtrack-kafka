55c131844fe8385dc29f3cb0a14a04d8e02b503d
k.cherepanov <k.cherepanov@sberauto.com>
2025-04-23 22:30:39 +0700

    fix

--- consumer/decoder.go
+++ consumer/decoder.go
@@ -8,14 +8,14 @@ import (
 	"github.com/ThreeDotsLabs/watermill/message"
 )
 
-type TypedHandler[T any] func(ctx context.Context, payload T) error
+type TypedHandler[T any] func(ctx context.Context, payload *T) error
 
-func JSONHandler[T any](handler TypedHandler[T]) func(msg *message.Message) error {
+func JSONHandler[T any](handler TypedHandler[T]) message.NoPublishHandlerFunc {
 	return func(msg *message.Message) error {
 		var payload T
 		if err := json.Unmarshal(msg.Payload, &payload); err != nil {
 			return fmt.Errorf("failed to unmarshal message: %w", err)
 		}
-		return handler(context.Background(), payload)
+		return handler(context.Background(), &payload)
 	}
 }
